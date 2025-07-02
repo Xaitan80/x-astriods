cec64e0823a7e1e5916b049ab421789039839706
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-02 21:36:20 +0200

    black screen

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill("black")
+                
 
 if __name__ == "__main__":
     main()
