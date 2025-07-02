8db32bfdbe04e9eb1b64186c98f1194d3291606a
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-03 00:03:51 +0200

    final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -9,14 +10,20 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.draw(screen)
         screen.fill("black")
         pygame.display.flip()
         dt = my_clock.tick(60) / 1000
+        
+        
+
+        
 
     
                 
