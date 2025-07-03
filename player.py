e9b789af9634ad5b2c4407fca994f51d4c651a95
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-03 09:36:22 +0200

    I can move even more

--- player.py
+++ player.py
@@ -22,12 +22,29 @@ class Player(CircleShape):
     def rotate(self, dt):
         rotation_delta = PLAYER_TURN_SPEED * dt
         self.rotation = self.rotation + rotation_delta
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
         
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(-dt)
+
+            self.rotate(-dt) #rotate left
+
         if keys[pygame.K_d]:
-            self.rotate(dt)
+
+            self.rotate(dt) #rotate right
+        
+        if keys[pygame.K_w]:
+
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+
+            self.move(-dt)
             # ?
+
+    
